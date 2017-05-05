5946b932741e6a2459510d0a2259c6610721ea07
Kevin Doyle <C13547287@mydit.ie>
2017-05-05 15:43:26 +0100

    Add boid with orbit method

---
+++ C13547287/Assets/Scripts/Boid.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector3 force, velocity, acceleration;
+
+    public float speed;
+
+    public float theta, thetaInc, radius;
+
+	void Start ()
+    {
+		
+	}
+
+    public Vector3 Orbit()
+    {
+        theta += thetaInc * (Time.deltaTime / 10.0f) * speed;
+
+        Vector3 desired = new Vector3(Mathf.Sin(theta), transform.position.y, -Mathf.Cos(theta));
+        desired *= radius;
+
+        return desired - velocity;
+    }
+	
+	void Update ()
+    {
+        force = Vector3.zero;
+        force += Orbit();
+        
+        transform.position = force;
+	}
+}
